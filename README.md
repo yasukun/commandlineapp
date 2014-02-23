d5d68c6f1ce095bc014a3df5043a95110f7a24cb
yasukun <masa.yasu.kun@gmail.com>
2014-02-23 20:38:15 +0900

    README modify

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ commandlineapp
 ==============
 
 leiningen template for command line app projects
+
+## Usage
+
+Create project:
+
+```bash
+lein new commandlineapp my-project
+```
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject commandlineapp/lein-template "0.1.0-SNAPSHOT"
   :description "leiningen template for command line app projects"
-  :url ""
+  :url "https://github.com/yasukun/commandlineapp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
